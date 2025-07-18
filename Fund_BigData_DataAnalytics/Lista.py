01065bd12de915919b0156a9e19f8371a69f8e40
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-07-18 16:34:24 -0300

    Update Lista.py

--- Fund_BigData_DataAnalytics/Lista.py
+++ Fund_BigData_DataAnalytics/Lista.py
@@ -3,3 +3,6 @@
 Uma lista é uma estrutura de dados em Python que permite armazenar uma coleção de itens. 
 Comandos:
 """
+# Criando uma lista com alguns itens
+#lista = [1, 2, 3, 4, 5]
+#print(lista)
