1eeeb1989be801a3bad78b1a0178f8b004916d63
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-07-26 18:18:56 -0300

    Update Tupla.py

--- Fund_BigData_DataAnalytics/Tupla.py
+++ Fund_BigData_DataAnalytics/Tupla.py
@@ -8,37 +8,29 @@ que permite armazenar uma coleção de itens.
 # Criando uma tupla com alguns itens
 minha_tupla = (1, 2, 3, 4, 5)# ou minha_tupla = 1,2,3,4,5
 print(minha_tupla)
-'''
 
-'''
+
 # Imprimindo o tamanho da tupla
 print(len(minha_tupla)) # Output: 5
-'''
 
-'''
+
 # Acessando um item específico na tupla
 print(minha_tupla[2]) # Output: 3
-'''
 
-'''
+
 # Verificando se um item está na tupla
 print(4 in minha_tupla) # Output: True
-'''
 
-'''
+
 # Concatenando duas tuplas
 outra_tupla = (6, 7, 8)
 minha_tupla = minha_tupla + outra_tupla
 print(minha_tupla)
-'''
 
-'''
+
 # Fatiando uma tupla
 parte_da_tupla = minha_tupla[2:5]
 print(parte_da_tupla)
 '''
 
-'''
-
-'''
 
