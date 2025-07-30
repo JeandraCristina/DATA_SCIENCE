f583ce2c34057b16156bc13b4c30ddf02cd85258
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-07-30 18:07:07 -0300

    Create Matplotlib.py

---
+++ Fundamentos_Data_Science/Matplotlib.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot as plt
+
+'''
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+pib = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+
+# Crie um grafico de linhas, anos no eixo x, pib no eixo y
+plt.plot(years, pib, color='green', marker='o', linestyle='solid') 
+
+#adicione um titulo
+plt.title("Nominal PIB")
+
+#adicione um rotulo ao eixo y
+plt.ylabel("Bilhões de Dolares")
+plt.show()
+'''
