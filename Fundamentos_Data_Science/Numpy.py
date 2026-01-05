c76469f21b8fce334c67f219ed6da88d67c4ee95
Jeandra Cristina <jeandracristinasr@gmail.com>
2026-01-05 17:41:32 -0300

    Update Numpy.py

---
+++ Fundamentos_Data_Science/Numpy.py
@@ -0,0 +1,4 @@
+# Python_NumPy:
+"""
+O NumPy é uma das bibliotecas mais populares do Python para
+computação numérica. Ele fornece uma estrutura de array multidimensional rápida e eficiente, bem como funções matemáticas para trabalhar com esses arrays. Utilizada para ciência de dados em aprendizado de máquina.
