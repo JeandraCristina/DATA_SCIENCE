bbc4c90fdcca1cbf45c9f7dde166f500105daa60
Jeandra Cristina <jeandracristinasr@gmail.com>
2026-02-21 21:17:03 -0300

    Update Pandas.py

--- Fundamentos_Data_Science/Pandas.py
+++ Fundamentos_Data_Science/Pandas.py
@@ -260,5 +260,6 @@ texto = f"A soma é: {Soma}\nA média é: {Média}\nA mediana é: {Mediana}\nO m
 # Salvar o texto em um arquivo .txt 
 with open('relatorio.txt', 'w') as arquivo:
      arquivo.write(texto)
+     print("Relatório salvo em 'relatorio.txt'")
 
                                     
