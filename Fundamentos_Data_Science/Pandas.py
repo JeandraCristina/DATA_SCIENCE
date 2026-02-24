34b9c4989a780eddb138fe5c43eab5c2ab9f99f2
Jeandra Cristina <jeandracristinasr@gmail.com>
2026-02-24 16:56:06 -0300

    Update Pandas.py

--- Fundamentos_Data_Science/Pandas.py
+++ Fundamentos_Data_Science/Pandas.py
@@ -289,5 +289,7 @@ Contagem = df["Valor"].count()
 # Criar o texto
 texto = f"A soma é: {Soma}\nA média é: {Média}\nA mediana é: {Mediana}\nO maior valor é: {Máximo}\nO menor valor é: {Mínimo}\nO total de números é: {Contagem}"
 
+# Salvar o texto em um arquivo .txt
+
 
                                     
