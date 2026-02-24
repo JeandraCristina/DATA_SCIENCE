5cc5ce5ba6603623bf7d53c592256f0ae3091b42
Jeandra Cristina <jeandracristinasr@gmail.com>
2026-02-24 16:56:26 -0300

    Update Pandas.py

--- Fundamentos_Data_Science/Pandas.py
+++ Fundamentos_Data_Science/Pandas.py
@@ -290,6 +290,7 @@ Contagem = df["Valor"].count()
 texto = f"A soma é: {Soma}\nA média é: {Média}\nA mediana é: {Mediana}\nO maior valor é: {Máximo}\nO menor valor é: {Mínimo}\nO total de números é: {Contagem}"
 
 # Salvar o texto em um arquivo .txt
+with open('relatorio.txt', 'w') as arquivo:
 
 
                                     
