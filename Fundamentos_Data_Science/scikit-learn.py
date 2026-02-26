c470c932bec480c534e1ab17da7dabdb38d13f95
Jeandra Cristina <jeandracristinasr@gmail.com>
2026-02-26 18:08:10 -0300

    Update scikit-learn.py

--- Fundamentos_Data_Science/scikit-learn.py
+++ Fundamentos_Data_Science/scikit-learn.py
@@ -6,3 +6,4 @@ Ele fornece ferramentas para criar, treinar e avaliar modelos em várias áreas 
 de máquina. Vou explicar brevemente como o scikit-learn pode ser usado em cada uma dessas tarefas:
 Aprendizado de Máquina: O scikit-learn oferece muitas funcionalidades para criar e treinar
 modelos de aprendizado de máquina. Você pode criar modelos que se ajustam a dados e fazem
+previsões ou classificações com base nesses dados. Ele fornece uma ampla gama de algoritmos e técnicas para isso.
