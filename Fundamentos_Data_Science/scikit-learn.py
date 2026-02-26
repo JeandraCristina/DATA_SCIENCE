bc96f031590ddfe9ff4c1b020c5ef07366acd17b
Jeandra Cristina <jeandracristinasr@gmail.com>
2026-02-26 18:06:27 -0300

    Update scikit-learn.py

--- Fundamentos_Data_Science/scikit-learn.py
+++ Fundamentos_Data_Science/scikit-learn.py
@@ -1,3 +1,4 @@
 #SCIKIT-LEARN
 """
 O scikit-learn é uma biblioteca muito versátil que abrange diversas tarefas relacionadas
+a aprendizado de máquina, incluindo regressão, classificação e clusterização, entre outras.
