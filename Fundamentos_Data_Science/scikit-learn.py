808c0233128d87e4f5e900ee0eefa52cf5bec3fa
Jeandra Cristina <jeandracristinasr@gmail.com>
2026-03-04 20:35:10 -0300

    Update scikit-learn.py

--- Fundamentos_Data_Science/scikit-learn.py
+++ Fundamentos_Data_Science/scikit-learn.py
@@ -112,5 +112,5 @@ A máquina fez suas adivinhações, e os cientistas compararam suas previsões c
 flores surpresa. A máquina acertou a maioria das vezes!
 Os cientistas ficaram maravilhados com a precisão da máquina. Eles usaram uma mágica especial
 chamada "Acurácia" para medir o quão boa a máquina estava em adivinhar. Eles viram que a
-chamada "Acurácia" para medir o quão boa a máquina estava em adivinhar. Eles viram que a
+máquina tinha uma precisão mágica de aproximadamente 83%, ou seja, ela acertou 83% das vezes!
 
