24b55389715a3d5193b93b9e0deaef1d9fbabb3c
Jeandra Cristina <jeandracristinasr@gmail.com>
2026-03-06 18:56:22 -0300

    Update scikit-learn.py

--- Fundamentos_Data_Science/scikit-learn.py
+++ Fundamentos_Data_Science/scikit-learn.py
@@ -135,4 +135,6 @@ alvos = dados_iris.target  # Tipo de flor (0, 1 ou 2)
 
 # Dividimos os dados em conjuntos de treinamento e teste
 caracteristicas_treino, caracteristicas_teste, alvos_treino, alvos_teste = train_test_split(caracteristicas, alvos,test_size=0.2, random_state=42)
+# Criamos nossa máquina de classificação mágica (K-Nearest Neighbors)
+
                                                                                             
