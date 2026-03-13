2d228e3205d00c6c4280da2e921f74242f197f9d
Jeandra Cristina <jeandracristinasr@gmail.com>
2026-03-13 19:44:31 -0300

    Update scikit-learn.py

--- Fundamentos_Data_Science/scikit-learn.py
+++ Fundamentos_Data_Science/scikit-learn.py
@@ -210,3 +210,4 @@ com um conjunto de dados mágico!
 # Primeiro, importamos a biblioteca mágica chamada scikit-learn
 from sklearn.datasets import make_blobs  # Vamos criar um conjunto de dados mágico
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
