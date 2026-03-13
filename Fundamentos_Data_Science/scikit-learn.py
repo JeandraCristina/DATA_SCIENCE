1c59295a4f8bf8cf0d90e280def5cc24d0296772
Jeandra Cristina <jeandracristinasr@gmail.com>
2026-03-13 19:41:07 -0300

    Update scikit-learn.py

--- Fundamentos_Data_Science/scikit-learn.py
+++ Fundamentos_Data_Science/scikit-learn.py
@@ -201,5 +201,6 @@ para que todos pudessem vê-los.
 E assim, os cientistas mágicos olharam para o mapa e viram como os Pontos haviam sido agrupados
 em grupos mágicos. Eles ficaram maravilhados com a mágica do K-Means e como ele tinha entendido
 a magia dos Pontos.
+E essa é a história de como os cientistas mágicos usaram o K-Means do scikit-learn para desvendar
 
                                                                                             
